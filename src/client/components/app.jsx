71d0e6b6c74d80bdf212aae017dbd7008c6920b0
Edward Meehan <edward.meehan.808@gmail.com>
2019-07-26 17:04:18 -0700

    building react app

---
+++ src/client/components/app.jsx
@@ -0,0 +1,106 @@
+import React from 'react';
+
+import TimeStamp from './time-stamp';
+import UserSearch from './user-search';
+import UserIcon from './user-icon';
+import ClassIcon from './class-icon';
+
+// import server from './server';
+// const {animals} = server;
+// animals('hello mom')
+//     .then((e) => console.log(e))
+//     .catch((error) => console.log(error));
+
+export default function App() {
+  return (
+    <div className="app-content">
+      <section className="app-branding">
+        <div className="app-branding__logo">
+          <img src="https://via.placeholder.com/200x200" width="200" height="200" alt="gym logo"/>
+        </div>
+      </section>
+      <div className="app-controllers">
+        <button className="app-controllers__restart-btn">
+          <svg width="56px" height="56px" viewBox="0 0 56 56"><use xlinkHref="#icon-cancel"></use></svg>
+        </button>
+        <div className="app-controllers__timestamp">
+          <TimeStamp></TimeStamp>
+        </div>
+        <div className="app-controllers__views">
+          
+          {/* USER SELECTION */}
+          <section className="view view--user">
+            <div className="view__content">
+              <div className="view__top">
+                <div className="module">
+                  <div className="module__title">Please enter your last name or member number.</div>
+                  <UserSearch></UserSearch>
+                </div>
+              </div>
+              <div className="view__bottom">
+                <div className="module">
+                    <div className="module__title">Select your profile photo.</div>
+                    <div className="module__list">
+                      <UserIcon></UserIcon>
+                      <UserIcon></UserIcon>
+                      <UserIcon></UserIcon>
+                    </div>
+                  </div>
+              </div>
+            </div>
+          </section>
+
+          {/* CLASS SELECTION */}
+          <section className="view view--class">
+            <div className="module__list">
+              <ClassIcon></ClassIcon>
+              <ClassIcon></ClassIcon>
+              <ClassIcon></ClassIcon>
+              <ClassIcon></ClassIcon>
+              <ClassIcon></ClassIcon>
+            </div>
+          </section>
+
+          {/* CONFIRM SELECTION */}
+          <section className="view view--confirm">
+            <div className="view__content">
+              <div className="view__top">
+                <div className="module module--card module--split">
+                  <div className="module__left class">
+                    <div className="class__title">Boxing</div>
+                    <div className="class__time">9:00 - 13:00</div>
+                    <div className="class__instructor">John Smith</div>
+                    <div className="class__details">This is the boxing class details.</div>
+                  </div>
+                  <div className="module__right">
+                    <img className="user-image" src="https://via.placeholder.com/175x175" width="175" height="175" alt="user image"/>
+                  </div>
+                </div>
+              </div>
+              <div className="view__bottom">
+                
+                {/* PER CONFIRM */}
+                <div className="module">
+                  <div className="module__title">Confirm your class attendance.</div>
+                  <div className="module__list module__list--confirm">
+                    <div className="module module--card">
+                      <svg width="60px" height="60px" viewBox="0 0 56 56"><use xlinkHref="#icon-cancel"></use></svg>
+                    </div>
+                    <div className="module module--card">
+                      <svg width="60px" height="60px" viewBox="0 0 61 46"><use xlinkHref="#icon-confirm"></use></svg>
+                    </div>
+                  </div>
+                </div>
+
+                {/* POST CONFIRM */}
+                <div className="module">
+                  <div className="module__title">Thank you for attending class today!</div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,97 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TimeStamp from './components/time-stamp';
-import UserSearch from './components/user-search';
-import UserIcon from './components/user-icon';
-import ClassIcon from './components/class-icon';
-// import server from './server';
+import App from './components/app';
 import './styles.css';
 
-// const {animals} = server;
-
-// animals('hello mom')
-//     .then((e) => console.log(e))
-//     .catch((error) => console.log(error));
-
 ReactDOM.render(
-  <div className="app-content">
-    <section className="app-branding">
-      <div className="app-branding__logo">
-        <img src="https://via.placeholder.com/200x200" width="200" height="200" alt="gym logo"/>
-      </div>
-    </section>
-    <div className="app-controllers">
-      <button className="app-controllers__restart-btn">
-        <svg width="56px" height="56px" viewBox="0 0 56 56"><use xlinkHref="#icon-cancel"></use></svg>
-      </button>
-      <div className="app-controllers__timestamp">
-        <TimeStamp></TimeStamp>
-      </div>
-      <div className="app-controllers__views">
-        {/* <section className="view view--user">
-          <div className="view__content">
-            <div className="view__top">
-              <div className="module">
-                <div className="module__title">Please enter your last name or member number.</div>
-                <UserSearch></UserSearch>
-              </div>
-            </div>
-            <div className="view__bottom">
-              <div className="module">
-                  <div className="module__title">Select your profile photo.</div>
-                  <div className="module__list">
-                    <UserIcon></UserIcon>
-                    <UserIcon></UserIcon>
-                    <UserIcon></UserIcon>
-                  </div>
-                </div>
-            </div>
-          </div>
-        </section>
-        <section className="view view--class">
-          <div className="module__list">
-            <ClassIcon></ClassIcon>
-            <ClassIcon></ClassIcon>
-            <ClassIcon></ClassIcon>
-            <ClassIcon></ClassIcon>
-            <ClassIcon></ClassIcon>
-          </div>
-        </section> */}
-        <section className="view view--confirm">
-          <div className="view__content">
-            <div className="view__top">
-              <div className="module module--card module--split">
-                <div className="module__left class">
-                  <div className="class__title">Boxing</div>
-                  <div className="class__time">9:00 - 13:00</div>
-                  <div className="class__instructor">John Smith</div>
-                  <div className="class__details">This is the boxing class details.</div>
-                </div>
-                <div className="module__right">
-                  <img className="user-image" src="https://via.placeholder.com/175x175" width="175" height="175" alt="user image"/>
-                </div>
-              </div>
-            </div>
-            <div className="view__bottom">
-              {/* <div className="module">
-                <div className="module__title">Confirm your class attendance.</div>
-                <div className="module__list module__list--confirm">
-                  <div className="module module--card">
-                    <svg width="60px" height="60px" viewBox="0 0 56 56"><use xlinkHref="#icon-cancel"></use></svg>
-                  </div>
-                  <div className="module module--card">
-                    <svg width="60px" height="60px" viewBox="0 0 61 46"><use xlinkHref="#icon-confirm"></use></svg>
-                  </div>
-                </div>
-              </div> */}
-              <div className="module">
-                <div className="module__title">Thank you for attending class today!</div>
-              </div>
-            </div>
-          </div>
-        </section>
-      </div>
-    </div>
-  </div>,
+  <App></App>,
   document.getElementById('index')
 );
